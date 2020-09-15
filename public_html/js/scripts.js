107a64b67d76724b67d927ea58f39dacd81998cc
karosowka13 <sowa@student.agh.edu.pl>
2020-09-15 18:14:52 +0200

    put online

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,412 +1,253 @@
-(function ($) {
+!(function (e) {
 	"use strict";
-
-	/* Preloader */
-	$(window).on("load", function () {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $(".spinner-wrapper");
+	e(window).on("load", function () {
+		var o,
+			a = 500;
+		(o = e(".spinner-wrapper")),
 			setTimeout(function () {
-				preloader.fadeOut(preloaderFadeOutTime);
+				o.fadeOut(a);
 			}, 500);
-		}
-		hidePreloader();
-	});
-
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-	$(window).on("scroll load", function () {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
-	});
-
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function () {
-		$(document).on("click", "a.page-scroll", function (event) {
-			var $anchor = $(this);
-			$("html, body")
-				.stop()
-				.animate(
-					{
-						scrollTop: $($anchor.attr("href")).offset().top,
-					},
-					600,
-					"easeInOutExpo"
-				);
-			event.preventDefault();
+	}),
+		e(window).on("scroll load", function () {
+			e(".navbar").offset().top > 20
+				? e(".fixed-top").addClass("top-nav-collapse")
+				: e(".fixed-top").removeClass("top-nav-collapse");
+		}),
+		e(function () {
+			e(document).on("click", "a.page-scroll", function (o) {
+				var a = e(this);
+				e("html, body")
+					.stop()
+					.animate(
+						{ scrollTop: e(a.attr("href")).offset().top },
+						600,
+						"easeInOutExpo"
+					),
+					o.preventDefault();
+			});
+		}),
+		e(".navbar-nav li a").on("click", function (o) {
+			e(this).parent().hasClass("dropdown") ||
+				e(".navbar-collapse").collapse("hide");
+		}),
+		e("#rotating").Morphext({
+			animation: "fadeIn",
+			separator: ",",
+			speed: 2e3,
+			complete: function () {},
 		});
-	});
-
-	// closes the responsive menu on menu item click
-	$(".navbar-nav li a").on("click", function (event) {
-		if (!$(this).parent().hasClass("dropdown"))
-			$(".navbar-collapse").collapse("hide");
-	});
-
-	/* Rotating Text - Morphtext */
-	$("#rotating").Morphext({
-		// The [in] animation type. Refer to Animate.css for a list of available animations.
-		animation: "fadeIn",
-		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-		separator: ",",
-		// The delay between the changing of each phrase in milliseconds.
-		speed: 2000,
-		complete: function () {
-			// Called after the entrance animation is executed.
-		},
-	});
-
-	/* Card Slider - Swiper */
-	var cardSlider = new Swiper(".card-slider", {
-		autoplay: {
-			delay: 4000,
-			disableOnInteraction: false,
-		},
-		loop: true,
+	new Swiper(".card-slider", {
+		autoplay: { delay: 4e3, disableOnInteraction: !1 },
+		loop: !0,
 		navigation: {
 			nextEl: ".swiper-button-next",
 			prevEl: ".swiper-button-prev",
 		},
 		slidesPerView: 3,
 		spaceBetween: 20,
-		breakpoints: {
-			// when window is <= 992px
-			992: {
-				slidesPerView: 2,
-			},
-			// when window is <= 768px
-			768: {
-				slidesPerView: 1,
-			},
-		},
+		breakpoints: { 992: { slidesPerView: 2 }, 768: { slidesPerView: 1 } },
 	});
-
-	/* Lightbox - Magnific Popup */
-	$(".popup-with-move-anim").magnificPopup({
+	e(".popup-with-move-anim").magnificPopup({
 		type: "inline",
-		fixedContentPos: false /* keep it false to avoid html tag shift with margin-right: 17px */,
-		fixedBgPos: true,
+		fixedContentPos: !1,
+		fixedBgPos: !0,
 		overflowY: "auto",
-		closeBtnInside: true,
-		preloader: false,
-		midClick: true,
+		closeBtnInside: !0,
+		preloader: !1,
+		midClick: !0,
 		removalDelay: 300,
 		mainClass: "my-mfp-slide-bottom",
 	});
-
-	/* Filter - Isotope */
-	var $grid = $(".grid").isotope({
-		// options
+	var o = e(".grid").isotope({
 		itemSelector: ".element-item",
 		layoutMode: "fitRows",
 	});
-
-	// filter items on button click
-	$(".filters-button-group").on("click", "a", function () {
-		var filterValue = $(this).attr("data-filter");
-		$grid.isotope({ filter: filterValue });
-	});
-
-	// change is-checked class on buttons
-	$(".button-group").each(function (i, buttonGroup) {
-		var $buttonGroup = $(buttonGroup);
-		$buttonGroup.on("click", "a", function () {
-			$buttonGroup.find(".is-checked").removeClass("is-checked");
-			$(this).addClass("is-checked");
+	e(".filters-button-group").on("click", "a", function () {
+		var a = e(this).attr("data-filter");
+		o.isotope({ filter: a });
+	}),
+		e(".button-group").each(function (o, a) {
+			var t = e(a);
+			t.on("click", "a", function () {
+				t.find(".is-checked").removeClass("is-checked"),
+					e(this).addClass("is-checked");
+			});
 		});
-	});
-
-	/* Counter - CountTo */
 	var a = 0;
-	$(window).scroll(function () {
-		if ($("#counter").length) {
-			// checking if CountTo section exists in the page, if not it will not run the script and avoid errors
-			var oTop = $("#counter").offset().top - window.innerHeight;
-			if (a == 0 && $(window).scrollTop() > oTop) {
-				$(".counter-value").each(function () {
-					var $this = $(this),
-						countTo = $this.attr("data-count");
-					$({
-						countNum: $this.text(),
-					}).animate(
+	function t() {
+		e("#callMeForm")
+			.removeClass()
+			.addClass("shake animated")
+			.one(
+				"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+				function () {
+					e(this).removeClass();
+				}
+			);
+	}
+	function n(o, a) {
+		if (o) var t = "h3 text-center tada animated";
+		else t = "h3 text-center";
+		e("#lmsgSubmit").removeClass().addClass(t).text(a);
+	}
+	function s(o, a) {
+		if (o) var t = "h3 text-center tada animated";
+		else t = "h3 text-center";
+		e("#cmsgSubmit").removeClass().addClass(t).text(a);
+	}
+	function i() {
+		e("#privacyForm")
+			.removeClass()
+			.addClass("shake animated")
+			.one(
+				"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+				function () {
+					e(this).removeClass();
+				}
+			);
+	}
+	function r(o, a) {
+		if (o) var t = "h3 text-center tada animated";
+		else t = "h3 text-center";
+		e("#pmsgSubmit").removeClass().addClass(t).text(a);
+	}
+	e(window).scroll(function () {
+		if (e("#counter").length) {
+			var o = e("#counter").offset().top - window.innerHeight;
+			0 == a &&
+				e(window).scrollTop() > o &&
+				(e(".counter-value").each(function () {
+					var o = e(this),
+						a = o.attr("data-count");
+					e({ countNum: o.text() }).animate(
+						{ countNum: a },
 						{
-							countNum: countTo,
-						},
-						{
-							duration: 2000,
+							duration: 2e3,
 							easing: "swing",
 							step: function () {
-								$this.text(Math.floor(this.countNum));
+								o.text(Math.floor(this.countNum));
 							},
 							complete: function () {
-								$this.text(this.countNum);
-								//alert('finished');
+								o.text(this.countNum);
 							},
 						}
 					);
-				});
-				a = 1;
-			}
+				}),
+				(a = 1));
 		}
-	});
-
-	/* Move Form Fields Label When User Types */
-	// for input and textarea fields
-	$("input, textarea").keyup(function () {
-		if ($(this).val() != "") {
-			$(this).addClass("notEmpty");
-		} else {
-			$(this).removeClass("notEmpty");
-		}
-	});
-
-	/* Call Me Form */
-	$("#callMeForm")
-		.validator()
-		.on("submit", function (event) {
-			if (event.isDefaultPrevented()) {
-				// handle the invalid form...
-				lformError();
-				lsubmitMSG(false, "Please fill all fields!");
-			} else {
-				// everything looks good!
-				event.preventDefault();
-				lsubmitForm();
-			}
+	}),
+		e("input, textarea").keyup(function () {
+			"" != e(this).val()
+				? e(this).addClass("notEmpty")
+				: e(this).removeClass("notEmpty");
+		}),
+		e("#callMeForm")
+			.validator()
+			.on("submit", function (o) {
+				var a, s, i, r, l;
+				o.isDefaultPrevented()
+					? (t(), n(!1, "Please fill all fields!"))
+					: (o.preventDefault(),
+					  (a = e("#lname").val()),
+					  (s = e("#lphone").val()),
+					  (i = e("#lemail").val()),
+					  (r = e("#lselect").val()),
+					  (l = e("#lterms").val()),
+					  e.ajax({
+							type: "POST",
+							url: "php/callmeform-process.php",
+							data:
+								"name=" +
+								a +
+								"&phone=" +
+								s +
+								"&email=" +
+								i +
+								"&select=" +
+								r +
+								"&terms=" +
+								l,
+							success: function (o) {
+								"success" == o
+									? (e("#callMeForm")[0].reset(),
+									  n(!0, "Request Submitted!"),
+									  e("input").removeClass("notEmpty"))
+									: (t(), n(!1, o));
+							},
+					  }));
+			}),
+		e("#contactForm")
+			.validator()
+			.on("submit", function (o) {
+				var a, t, n, i;
+				o.isDefaultPrevented()
+					? (e("#contactForm")
+							.removeClass()
+							.addClass("shake animated")
+							.one(
+								"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+								function () {
+									e(this).removeClass();
+								}
+							),
+					  s(!1, "Please fill all fields!"))
+					: (o.preventDefault(),
+					  (a = e("#cname").val()),
+					  (t = e("#cemail").val()),
+					  (n = e("#cmessage").val()),
+					  (i = e("#cterms").val()),
+					  e.ajax({
+							type: "POST",
+							url: "php/mail.php",
+							data:
+								"&name=" + a + "&email=" + t + "&message=" + n + "&terms=" + i,
+							success: function (e) {
+								jQuery("#contactForm").html(
+									"<div id='email-sent'><p>Thank you for the message.</p><p>We will reply as soon as possible. </p></div>"
+								);
+							},
+							error: function (e) {
+								alert("Sendmail failed possibly php script: " + e);
+							},
+					  }));
+			}),
+		e("#privacyForm")
+			.validator()
+			.on("submit", function (o) {
+				var a, t, n, s;
+				o.isDefaultPrevented()
+					? (i(), r(!1, "Please fill all fields!"))
+					: (o.preventDefault(),
+					  (a = e("#pname").val()),
+					  (t = e("#pemail").val()),
+					  (n = e("#pselect").val()),
+					  (s = e("#pterms").val()),
+					  e.ajax({
+							type: "POST",
+							url: "php/privacyform-process.php",
+							data:
+								"name=" + a + "&email=" + t + "&select=" + n + "&terms=" + s,
+							success: function (o) {
+								"success" == o
+									? (e("#privacyForm")[0].reset(),
+									  r(!0, "Request Submitted!"),
+									  e("input").removeClass("notEmpty"))
+									: (i(), r(!1, o));
+							},
+					  }));
+			}),
+		e("body").prepend(
+			'<a href="body" class="back-to-top page-scroll">Back to Top</a>'
+		);
+	e(window).scroll(function () {
+		e(window).scrollTop() > 700
+			? e("a.back-to-top").fadeIn("500")
+			: e("a.back-to-top").fadeOut("500");
+	}),
+		e(".button, a, button").mouseup(function () {
+			e(this).blur();
 		});
-
-	function lsubmitForm() {
-		// initiate variables with form content
-		var name = $("#lname").val();
-		var phone = $("#lphone").val();
-		var email = $("#lemail").val();
-		var select = $("#lselect").val();
-		var terms = $("#lterms").val();
-
-		$.ajax({
-			type: "POST",
-			url: "php/callmeform-process.php",
-			data:
-				"name=" +
-				name +
-				"&phone=" +
-				phone +
-				"&email=" +
-				email +
-				"&select=" +
-				select +
-				"&terms=" +
-				terms,
-			success: function (text) {
-				if (text == "success") {
-					lformSuccess();
-				} else {
-					lformError();
-					lsubmitMSG(false, text);
-				}
-			},
-		});
-	}
-
-	function lformSuccess() {
-		$("#callMeForm")[0].reset();
-		lsubmitMSG(true, "Request Submitted!");
-		$("input").removeClass("notEmpty"); // resets the field label after submission
-	}
-
-	function lformError() {
-		$("#callMeForm")
-			.removeClass()
-			.addClass("shake animated")
-			.one(
-				"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-				function () {
-					$(this).removeClass();
-				}
-			);
-	}
-
-	function lsubmitMSG(valid, msg) {
-		if (valid) {
-			var msgClasses = "h3 text-center tada animated";
-		} else {
-			var msgClasses = "h3 text-center";
-		}
-		$("#lmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-	}
-
-	/* Contact Form */
-	$("#contactForm")
-		.validator()
-		.on("submit", function (event) {
-			if (event.isDefaultPrevented()) {
-				// handle the invalid form...
-				cformError();
-				csubmitMSG(false, "Please fill all fields!");
-			} else {
-				// everything looks good!
-				event.preventDefault();
-				csubmitForm();
-			}
-		});
-
-	function csubmitForm() {
-		// initiate variables with form content
-		var name = $("#cname").val();
-		var email = $("#cemail").val();
-		var message = $("#cmessage").val();
-		var terms = $("#cterms").val();
-		$.ajax({
-			type: "POST",
-			url: "php/mail.php",
-			data:
-				"&name=" +
-				name +
-				"&email=" +
-				email +
-				"&message=" +
-				message +
-				"&terms=" +
-				terms,
-			success: function (phpReturnResult) {
-				//alert('Success: ' + phpReturnResult);
-				jQuery("#contactForm").html(
-					"<div id='email-sent'><p>Thank you for the message.</p><p>We will reply as soon as possible. </p></div>"
-				);
-			},
-			error: function (errormessage) {
-				//you would not show the real error to the user - this is just to see if everything is working
-				alert("Sendmail failed possibly php script: " + errormessage);
-			},
-		});
-	}
-
-	function cformSuccess() {
-		$("#contactForm")[0].reset();
-		csubmitMSG(true, "Message Submitted!");
-		$("input").removeClass("notEmpty"); // resets the field label after submission
-		$("textarea").removeClass("notEmpty"); // resets the field label after submission
-	}
-
-	function cformError() {
-		$("#contactForm")
-			.removeClass()
-			.addClass("shake animated")
-			.one(
-				"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-				function () {
-					$(this).removeClass();
-				}
-			);
-	}
-
-	function csubmitMSG(valid, msg) {
-		if (valid) {
-			var msgClasses = "h3 text-center tada animated";
-		} else {
-			var msgClasses = "h3 text-center";
-		}
-		$("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-	}
-
-	/* Privacy Form */
-	$("#privacyForm")
-		.validator()
-		.on("submit", function (event) {
-			if (event.isDefaultPrevented()) {
-				// handle the invalid form...
-				pformError();
-				psubmitMSG(false, "Please fill all fields!");
-			} else {
-				// everything looks good!
-				event.preventDefault();
-				psubmitForm();
-			}
-		});
-
-	function psubmitForm() {
-		// initiate variables with form content
-		var name = $("#pname").val();
-		var email = $("#pemail").val();
-		var select = $("#pselect").val();
-		var terms = $("#pterms").val();
-
-		$.ajax({
-			type: "POST",
-			url: "php/privacyform-process.php",
-			data:
-				"name=" +
-				name +
-				"&email=" +
-				email +
-				"&select=" +
-				select +
-				"&terms=" +
-				terms,
-			success: function (text) {
-				if (text == "success") {
-					pformSuccess();
-				} else {
-					pformError();
-					psubmitMSG(false, text);
-				}
-			},
-		});
-	}
-
-	function pformSuccess() {
-		$("#privacyForm")[0].reset();
-		psubmitMSG(true, "Request Submitted!");
-		$("input").removeClass("notEmpty"); // resets the field label after submission
-	}
-
-	function pformError() {
-		$("#privacyForm")
-			.removeClass()
-			.addClass("shake animated")
-			.one(
-				"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-				function () {
-					$(this).removeClass();
-				}
-			);
-	}
-
-	function psubmitMSG(valid, msg) {
-		if (valid) {
-			var msgClasses = "h3 text-center tada animated";
-		} else {
-			var msgClasses = "h3 text-center";
-		}
-		$("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-	}
-
-	/* Back To Top Button */
-	// create the back to top button
-	$("body").prepend(
-		'<a href="body" class="back-to-top page-scroll">Back to Top</a>'
-	);
-	var amountScrolled = 700;
-	$(window).scroll(function () {
-		if ($(window).scrollTop() > amountScrolled) {
-			$("a.back-to-top").fadeIn("500");
-		} else {
-			$("a.back-to-top").fadeOut("500");
-		}
-	});
-
-	/* Removes Long Focus On Buttons */
-	$(".button, a, button").mouseup(function () {
-		$(this).blur();
-	});
 })(jQuery);
-
-/* change language script */
-
-//change text in paragraphs
 const language = {
 	eng: {
 		home: "HOME",
@@ -479,13 +320,14 @@ const language = {
 		seeyou: "HOPE TO SEE YOU SOON",
 		come: "COME EXPERIENCE OUR WORLD WITH US",
 		check: "Check also",
+		freewalkingtourH2: "FREE WALKING TOUR",
 	},
 	es: {
 		home: "HOME",
 		about: "SOBRE NOSOTROS",
 		doo: "QUE HACEMOS",
 		contact: "CONTACTOS",
-		freewalkingtour: "TOUR LIBRE A PIE",
+		freewalkingtour: "TOUR GRATIS A PIE",
 		navbarDropdown: "IDIOMA",
 		hi: "MALAGA ES ",
 		rotating: "MUSEOS, PARQUES & JARDINES, MONUMENTOS",
@@ -553,46 +395,29 @@ const language = {
 		seeyou: "ESPERAMOS VERTE PRONTO.",
 		come: "VEN A EXPERIMENTAR NUESTRO MUNDO CON NOSOTROS",
 		check: "Comprueba también",
+		freewalkingtourH2: "TOUR GRATIS A PIE",
 	},
 };
-
-//define language via window hash
-if (window.location.hash) {
-	if (window.location.hash === "#es") {
-		for (let id in language.es) {
-			document.getElementById(id).textContent = language.es[id];
-		}
-	}
-	// else if (window.location.hash === "#eng") {
-	// 	for (let id in language.eng) {
-	// 		document.getElementById(id).textContent = language.eng[id];
-	// 	}
-	// }
-}
-
+if (window.location.hash && "#es" === window.location.hash)
+	for (let e in language.es)
+		document.getElementById(e).textContent = language.es[e];
 $(window).on("hashchange", function () {
-	window.location.reload(true);
+	window.location.reload(!0);
 });
-
-/* disabled rigth clicking events */
 var message = "Function Disabled!";
-
 function clickIE4() {
-	if (event.button == 2) {
-		return false;
-	}
+	if (2 == event.button) return !1;
 }
 function clickNS4(e) {
-	if (document.layers || (document.getElementById && !document.all)) {
-		if (e.which == 2 || e.which == 3) {
-			return false;
-		}
-	}
+	if (
+		(document.layers || (document.getElementById && !document.all)) &&
+		(2 == e.which || 3 == e.which)
+	)
+		return !1;
 }
-if (document.layers) {
-	document.captureEvents(Event.MOUSEDOWN);
-	document.onmousedown = clickNS4;
-} else if (document.all && !document.getElementById) {
-	document.onmousedown = clickIE4;
-}
-document.oncontextmenu = new Function("return false");
+document.layers
+	? (document.captureEvents(Event.MOUSEDOWN), (document.onmousedown = clickNS4))
+	: document.all &&
+	  !document.getElementById &&
+	  (document.onmousedown = clickIE4),
+	(document.oncontextmenu = new Function("return false"));
